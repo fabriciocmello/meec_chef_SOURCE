03a2b26d4ece8a40fb8ae6c14ac7d67ccb12c5de
fabriciocmello <fabriciocmello@gmail.com>
2015-11-12 15:54:33 -0200

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -16,8 +16,8 @@
 
 define('WP_REDIS_HOST', 'meec.jpsdi1.0001.sae1.cache.amazonaws.com');
  
-define('WP_HOME', 'http://d3tymjhyc0ikh6.cloudfront.net/');
-define('WP_SITEURL', 'http://d3tymjhyc0ikh6.cloudfront.net/');
+define('WP_HOME', 'http://www.cupomzera.com.br/');
+define('WP_SITEURL', 'http://www.cupomzera.com.br/');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
